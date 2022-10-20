18f38220fe4d2b9d23058e57f95572ab01752a3b
LAPTOP-0BE72HIK\afifk <afifkhanab@gmail.com>
2022-10-20 21:42:38 +0530

    init

---
+++ full/script.js
@@ -0,0 +1,16 @@
+var timeLeft = 0;
+function startTimer(){
+    timeLeft = 60;
+}
+
+var elem = document.getElementById('timer');
+    setInterval(countdown, 1000);
+
+    function countdown() {
+        elem.innerText = timeLeft;
+        if(timeLeft>=1)
+        {
+            timeLeft--;
+        }
+
+    }
